8005b7bd538ece5d706b4deb66cd31e4bf53924d
Amirreza Taghizadeh <taghizadeh.a.2015@gmail.com>
2024-09-03 16:35:25 +0330

    first commit

---
+++ build/Desktop_Qt_6_7_2_MSVC2019_64bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_appCustomBarChart_FILE C:/Users/Asus/Projects/CustomBarChart/build/Desktop_Qt_6_7_2_MSVC2019_64bit-Debug/appCustomBarChart.exe)
+set(__QT_DEPLOY_TARGET_appCustomBarChart_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_appCustomBarChart_RUNTIME_DLLS C:/Qt/6.7.2/msvc2019_64/bin/Qt6Quickd.dll;C:/Qt/6.7.2/msvc2019_64/bin/Qt6QmlModelsd.dll;C:/Qt/6.7.2/msvc2019_64/bin/Qt6Qmld.dll;C:/Qt/6.7.2/msvc2019_64/bin/Qt6Networkd.dll;C:/Qt/6.7.2/msvc2019_64/bin/Qt6OpenGLd.dll;C:/Qt/6.7.2/msvc2019_64/bin/Qt6Guid.dll;C:/Qt/6.7.2/msvc2019_64/bin/Qt6Cored.dll)
